f657ca426225570bc06df66b9a47d2e177d0c2b0
Edoardo Di Chiara <dichiaraedoardo@gmail.com>
2023-03-17 16:57:13 +0100

    Update Question.kt

--- app/src/main/java/com/example/jettrivia/component/Question.kt
+++ app/src/main/java/com/example/jettrivia/component/Question.kt
@@ -2,21 +2,33 @@ package com.example.jettrivia.component
 
 import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
-import androidx.compose.ui.text.*
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.jettrivia.model.QuestionItem
 import com.example.jettrivia.screens.QuestionsViewModel
 import com.example.jettrivia.util.AppColors
 
@@ -35,9 +47,16 @@ fun Questions(viewModel: QuestionsViewModel) {
     }
 }
 
-@Preview
 @Composable
-fun QuestionDisplay() {
+fun QuestionDisplay(
+    question: QuestionItem,
+    questionIndex: MutableState<Int>,
+    viewModel: QuestionsViewModel,
+    onNextClick: (Int) -> Unit
+) {
+    val choicesState = remember(question) {
+        question.choices.toMutableList()
+    }
     val pathEffect = PathEffect.dashPathEffect(intervals = floatArrayOf(10f, 10f), 0f)
     Surface(
         modifier = Modifier
@@ -53,15 +72,58 @@ fun QuestionDisplay() {
         ) {
             QuestionTracker()
             DrawDottedLine(pathEffect = pathEffect)
+            Column {
+                Text(
+                    text = "CIJSFIJFGGJ",
+                    fontSize = 17.sp,
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 22.sp,
+                    color = AppColors.mOffWhite,
+                    modifier = Modifier
+                        .padding(6.dp)
+                        .align(alignment = Alignment.Start)
+                        .fillMaxHeight(fraction = 0.3f)
+                )
+                choicesState.forEachIndexed { index, answertext ->
+                    Row(
+                        modifier = Modifier
+                            .padding(3.dp)
+                            .fillMaxWidth()
+                            .height(45.dp)
+                            .border(
+                                width = 4.dp,
+                                brush = Brush.linearGradient(
+                                    colors = listOf(
+                                        AppColors.mDarkPurple,
+                                        AppColors.mDarkPurple
+                                    )
+                                ),
+                                shape = RoundedCornerShape(15.dp)
+                            )
+                            .clip(
+                                shape = RoundedCornerShape(
+                                    topStartPercent = 50,
+                                    topEndPercent = 50,
+                                    bottomEndPercent = 50,
+                                    bottomStartPercent = 50
+                                )
+                            ).background(color = Color.Transparent)
+                    ) {
+
+                    }
+                }
+            }
         }
     }
 }
 
 @Composable
 fun DrawDottedLine(pathEffect: PathEffect) {
-    Canvas(modifier = Modifier
-        .fillMaxWidth()
-        .height(1.dp)) {
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+    ) {
         drawLine(
             color = AppColors.mLightGray,
             start = Offset(x = 0f, y = 0f),
